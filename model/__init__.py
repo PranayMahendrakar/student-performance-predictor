67b7a90489dd3dbf9975f7add74c226adeb47430
Pranay M Mahendrakar <90593932+PranayMahendrakar@users.noreply.github.com>
2026-03-06 22:10:26 +0530

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+"""Student Performance Predictor - Model Package"""
+from .predictor import StudentPredictor, FEATURE_COLUMNS, FEATURE_LABELS
+from .predictor import PERFORMANCE_COLORS, PERFORMANCE_ICONS
+
+__all__ = [
+    "StudentPredictor",
+    "FEATURE_COLUMNS",
+    "FEATURE_LABELS",
+    "PERFORMANCE_COLORS",
+    "PERFORMANCE_ICONS",
+]
+
